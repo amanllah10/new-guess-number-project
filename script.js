b1a65bc13d24e2bfbd6b3ab602dc7fa042fc4d8e
Your Name <your.email@example.com>
2025-08-20 04:30:21 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let userInput = document.querySelector('.user-input')
+let seeBtn = document.querySelector('.see-btn')
+let displayText = document.querySelector('.guess-h1')
+
+let computerGenerater = Math.floor(Math.random() * 100)
+console.log(computerGenerater)
+
+seeBtn.addEventListener('click', () => {
+    if (userInput.value > computerGenerater) {
+        displayText.style.color = 'red' 
+        displayText.innerHTML = 'Your Guess Number is too High from Computer Guess'
+    } else if (userInput.value < computerGenerater) {
+        displayText.style.color = 'blue' 
+        displayText.innerHTML = 'Your Guess Number is too low from Computer Guess'
+    } else if (userInput.value = computerGenerater) {
+        displayText.style.color = 'green' 
+        displayText.innerHTML = 'Congratulation You Win Your Guess Number is Equal to Computer Guess'
+    }
+
+})
